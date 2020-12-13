7f340ba5d1c1c4d3554feb47f48aefe4f78c79c0
Guy Bertental <guybartal@gmail.com>
2020-12-13 21:36:54 +0200

    fix bugs in classifier

--- Jetson.ONNX/classifier.py
+++ Jetson.ONNX/classifier.py
@@ -42,91 +42,78 @@ def gstreamer_pipeline(
     )
 
 
-class Model(object):
-    def __init__(self):
-        # placeholder for the onnx session
-        self.session = None
+def get_model_and_sig(model_dir):
+    """Method to get name of model file. Assumes model is in the parent directory for script."""
+    with open(os.path.join(model_dir, "../signature.json"), "r") as f:
+        signature = json.load(f)
+    
+    model_file = os.path.join(model_dir, "../" + signature.get("filename"))
+    print (model_file)
+    if not os.path.isfile(model_file):
+        raise FileNotFoundError(f"Model file does not exist")
+    return model_file, signature
 
-    def load(self, model_dir):
-        """Load the model from path to model file"""
-        signature_path = os.path.join(model_dir, "/signature.json")
-        print(f"loading signature file {signature_path}")
-        with open(signature_path, "r") as f:
-            self.signature = json.load(f)
-        model_file = "/" + self.signature.get("filename")
-        if not os.path.isfile(model_file):
-            raise FileNotFoundError(f"Model file does not exist")
+def load_model(model_file):
+    """Load the model from path to model file"""
+    # Load ONNX model as session.
+    return rt.InferenceSession(path_or_bytes=model_file)
 
-        # Load ONNX model as session.
-        self.session = rt.InferenceSession(path_or_bytes=model_file)
+def get_prediction(image, session, signature):
+    """
+    Predict with the ONNX session!
+    """
+    # get the signature for model inputs and outputs
+    signature_inputs = signature.get("inputs")
+    signature_outputs = signature.get("outputs")
 
-        # get the signature for model inputs and outputs
-        self.signature_inputs = signature.get("inputs")
-        self.signature_outputs = signature.get("outputs")
+    if "Image" not in signature_inputs:
+        raise ValueError("ONNX model doesn't have 'Image' input! Check signature.json, and please report issue to Lobe.")
 
-        if "Image" not in self.signature_inputs:
-            raise ValueError("ONNX model doesn't have 'Image' input! Check signature.json, and please report issue to Lobe.")
+    # process image to be compatible with the model
+    img = process_image(image, signature_inputs.get("Image").get("shape"))
 
-    def predict(self, image):
-        """
-        Predict with the ONNX session!
-        """
-        # process image to be compatible with the model
-        img = self.process_image(image, self.signature_inputs.get("Image").get("shape"))
+    # run the model!
+    fetches = [(key, value.get("name")) for key, value in signature_outputs.items()]
+    # make the image a batch of 1
+    feed = {signature_inputs.get("Image").get("name"): [img]}
+    outputs = session.run(output_names=[name for (_, name) in fetches], input_feed=feed)
+    # un-batch since we ran an image with batch size of 1,
+    # convert to normal python types with tolist(), and convert any byte strings to normal strings with .decode()
+    results = {}
+    for i, (key, _) in enumerate(fetches):
+        val = outputs[i].tolist()[0]
+        if isinstance(val, bytes):
+            val = val.decode()
+        results[key] = val
 
-        # run the model!
-        fetches = [(key, value.get("name")) for key, value in self.signature_outputs.items()]
-        # make the image a batch of 1
-        feed = {signature_inputs.get("Image").get("name"): [img]}
-        outputs = session.run(output_names=[name for (_, name) in fetches], input_feed=feed)
-        # un-batch since we ran an image with batch size of 1,
-        # convert to normal python types with tolist(), and convert any byte strings to normal strings with .decode()
-        results = {}
-        for i, (key, _) in enumerate(fetches):
-            val = outputs[i].tolist()[0]
-            if isinstance(val, bytes):
-                val = val.decode()
-            results[key] = val
+    return results
 
-        return results
+def process_image(image, input_shape):
+    """
+    Given a PIL Image, center square crop and resize to fit the expected model input, and convert from [0,255] to [0,1] values.
+    """
+    width, height = image.size
+    # ensure image type is compatible with model and convert if not
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    # center crop image (you can substitute any other method to make a square image, such as just resizing or padding edges with 0)
+    if width != height:
+        square_size = min(width, height)
+        left = (width - square_size) / 2
+        top = (height - square_size) / 2
+        right = (width + square_size) / 2
+        bottom = (height + square_size) / 2
+        # Crop the center of the image
+        image = image.crop((left, top, right, bottom))
+    # now the image is square, resize it to be the right shape for the model input
+    input_width, input_height = input_shape[1:3]
+    if image.width != input_width or image.height != input_height:
+        image = image.resize((input_width, input_height))
 
-
-    def process_image(self, image, input_shape):
-        """
-        Given a PIL Image, center square crop and resize to fit the expected model input, and convert from [0,255] to [0,1] values.
-        """
-        width, height = image.size
-        # ensure image type is compatible with model and convert if not
-        if image.mode != "RGB":
-            image = image.convert("RGB")
-        # center crop image (you can substitute any other method to make a square image, such as just resizing or padding edges with 0)
-        if width != height:
-            square_size = min(width, height)
-            left = (width - square_size) / 2
-            top = (height - square_size) / 2
-            right = (width + square_size) / 2
-            bottom = (height + square_size) / 2
-            # Crop the center of the image
-            image = image.crop((left, top, right, bottom))
-        # now the image is square, resize it to be the right shape for the model input
-        input_width, input_height = input_shape[1:3]
-        if image.width != input_width or image.height != input_height:
-            image = image.resize((input_width, input_height))
-
-        # make 0-1 float instead of 0-255 int (that PIL Image loads by default)
-        image = np.asarray(image) / 255.0
-        # format input as model expects
-        return image.astype(np.float32)
-
-    def cleanup(self):
-        # close our session if one exists
-        if self.session is not None:
-            self.session.close()
-            self.session = None
-
-    def __del__(self):
-        self.cleanup()
-
+    # make 0-1 float instead of 0-255 int (that PIL Image loads by default)
+    image = np.asarray(image) / 255.0
+    # format input as model expects
+    return image.astype(np.float32)
 
 def arg_parse():
     parser = argparse.ArgumentParser(description="Predict labels from csi camera.")
@@ -137,9 +124,9 @@ def arg_parse():
 
 if __name__ == "__main__":
     args = arg_parse()
-    print("Loading Model")
-    model = Model()
-    model.load(args.model_dir)
+    print(f"Loading Model from {args.model_dir}")
+    model_file, signature = get_model_and_sig(args.model_dir)
+    session = load_model(model_file)
 
     print("Starting Video Capture")  
     #for usb camera unmark this line and mark gstreamer
@@ -151,16 +138,15 @@ if __name__ == "__main__":
     font = cv2.FONT_HERSHEY_SIMPLEX
     timeA = time.time()
     while True:
-
         ret,image = videoCapture.read()
         image_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image_pil = Image.fromarray(image_RGB)
 
-        outputs = model.predict(image_pil)
-        print(f"Predicted: {outputs}")
+        prediction = get_prediction(image_pil, session, signature)
+        print(f"Predicted: {prediction}")
 
         if (i%30==0):
-            cv2.putText(image,str(outputs),(20,20), font, .5,(255,255,255),2,cv2.LINE_AA)
+            cv2.putText(image,str(prediction),(20,20), font, .5,(255,255,255),2,cv2.LINE_AA)
             print (f'storing file {args.output_dir}/frame{i}.jpg')
             cv2.imwrite(f'{args.output_dir}/frame{i}.jpg',image)
         i+=1
